409eb8f725fced04adda3df1f3a506b5567127be
Oebele <Oebele_ishopsakee@hotmail.com>
2016-11-24 20:20:39 +0100

    Update BingyFy.py

--- BingyFy.py
+++ BingyFy.py
@@ -1,11 +1,12 @@
 '''
-Created on May 11, 2016
+Created on Nov 24, 2016
 
-@author: x
+@author: oebele
 '''
 from urllib.request import urlopen
 from re import findall, sub
 import os
+import time
 
 
 def getPictureUrl(url):
@@ -30,12 +31,25 @@ def createPictureName(directory, url):
     return directory + os.sep + url.split(os.sep)[-1]
 
 
+def deleteOldPictures(picture_path):
+    current_time = time.time()
+    
+    
+    for f in os.listdir(picture_path):
+        path = picture_path + '/' + f
+        creation_time = os.path.getctime(path)
+        extension = os.path.splitext(path)[1]
+        if extension != '.py' and (current_time - creation_time) / (24 * 3600) > 7:
+            os.unlink(path)
+            print('{} removed'.format(f))
+
 def main():
     url = "http://bing.com"
     cmd = "gsettings set org.gnome.desktop.background picture-uri 'file:///{}'"
     picture_path = os.environ["HOME"] + os.sep + '/Bing Pictures/'
     picture_url = getPictureUrl(url)
     picture_name = createPictureName(picture_path, picture_url)
+    deleteOldPictures(picture_path)
     picture = getPicture(picture_url)
     command = cmd.format(picture_name)
 
